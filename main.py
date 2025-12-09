b2cfae6e550e75f6c5d4efd22673cb2d4504db84
trustworthyaxolotl26 <efreeman2029@kehillahstudent.org>
2025-12-09 10:24:35 -0800

    working on stuff and func description thingys

--- main.py
+++ main.py
@@ -12,21 +12,34 @@ r_sence = robot.right_sonar()
 def nav_wall():
     global l_sence
     global r_sence
+    l_sence = robot.left_sonar()
+    r_sence = robot.right_sonar()
     while l_sence and r_sence > 11:
         l_sence = robot.left_sonar()
         r_sence = robot.right_sonar()
         robot.motors(FORWARD, FORWARD, 0.1)
-        print(f"left = {robot.left_sonar()}, right = {robot.right_sonar()}")
+        #print(f"left = {robot.left_sonar()}, right = {robot.right_sonar()}")
     robot.motors(STOP, STOP, 0.1)
 
 def turn_perf():
+    '''
+    this funchtion is supposed to turn a perfect 360 degrees but it tkes FOREVBERRR!!! UGGGGHHHHHHHHHHHH
+    '''
     global l_sence
     global r_sence
-    original_right_sence = r_sence
-    original_left_sence = l_sence
-    while l_sence and r_sence != original_left_sence and original_r_sence:
-        robot.motors(FORWARD, FORWARD, 0.1)
+    original_right_sence = robot.right_sonar()
+    original_left_sence = robot.left_sonar()
+    print(f"{l_sence, r_sence} <curent, og> {original_right_sence, original_left_sence} ")
+    robot.motors(FORWARD, BACKWARD, 0.2)
+    l_sence = robot.left_sonar()
+    r_sence = robot.right_sonar()
+    while l_sence != original_left_sence or r_sence != original_right_sence:
+        robot.motors(FORWARD, BACKWARD, 1)
+        l_sence = robot.left_sonar()
+        r_sence = robot.right_sonar()
+        print(f"{l_sence, r_sence} <curent, og> {original_right_sence, original_left_sence} ")
     robot.motors(STOP, STOP, 0.1)
+    on()
 
 def off():
     print("the robot is off")
@@ -47,13 +60,15 @@ def on():
     elif press == "sp":
         spiral()
     elif press == "w":
-        weave()
+        inter_weave()
     elif press == "c":
         circle()
     elif press == "se":
         sence()
     elif press == "h":
         on()
+    elif press == "pain":
+        turn_perf()
     else:
         again()
 
@@ -61,93 +76,56 @@ def spiral():
     print("the robot is going spiral")
     global l_sence
     global r_sence
-    press = input("would you like to: turn it off(o), spiral again(sp), weave(w), circle(c), sence(se), or hold(h)?")
-    if press == "o":
-        off()
-    elif press == "sp":
-        spiral()
-    elif press == "w":
-        weave()
-    elif press == "c":
-        circle()
-    elif press == "se":
-        sence()
-    elif press == "h":
-        on()
-    else:
-        again()
+    l_sence = robot.left_sonar()
+    r_sence = robot.right_sonar()
+    nav_wall()
+    robot.motors(FORWARD, BACKWARD, 1.49999999999999999999)
+    nav_wall()
 
-def weave():
+    on()
+
+def weave(way):
     global l_sence
     global r_sence
-    way = input("would you like a long weve(l) or a short weave?(s)")
-    print("the robot is goin weave")
+    l_sence = robot.left_sonar()
+    r_sence = robot.right_sonar()
     if way == "l":
         print("long, ok")
         nav_wall()
-        robot.motors(FORWARD, BACKWARD, 1.499999999999999999999)
+        robot.motors(FORWARD, BACKWARD, 1.525)
         print("now to weave")
     elif way == "s":
         print("shork, got ti")
-    press = input("would you like to: turn it off(o), spiral(sp), weave again(w), circle(c), sence(se), or hold(h)?")
-    if press == "o":
-        off()
-    elif press == "sp":
-        spiral()
-    elif press == "w":
-        weave()
-    elif press == "c":
-        circle()
-    elif press == "se":
-        sence()
-    elif press == "h":
-        on()
-    else:
-        again()
+    on()
 
-# 90 = 1.525
+def inter_weave():
+    '''
+    so i can make a fuction with a variable
+    '''
+    way = input("would you like a long weve(l) or a short weave?(s)")
+    print("the robot is goin weave")
+    if way == "l":
+        weave("l")
+    elif way == "s":
+        weave("s")
+
+# 90 = 1.525 ???
 # ~360 = 6.124999999999999
 
 def circle():
     print("the robot is goinf circle")
     robot.motors(FORWARD, BACKWARD, 4*1.49999999999999999999)
-    press = input("would you like to: turn it off(o), spiral(sp), weave(w), circle again(c), sence,(se), or hold(h)?")
-    if press == "o":
-        off()
-    elif press == "sp":
-        spiral()
-    elif press == "w":
-        weave()
-    elif press == "c":
-        circle()
-    elif press == "se":
-        sence()
-    elif press == "h":
-        on()
-    else:
-        again()
+    on()
 
 def sence():
     print("the robot is going sence")
     global l_sence
     global r_sence
+    l_sence = robot.left_sonar()
+    r_sence = robot.right_sonar()
     print(f"the left sonar sences {l_sence} cm from the wall")
     print(f"the right sonar sences {r_sence}")
-    press = input("would you like to: turn it off(o), spiral(sp), weave(w), circle(c), sence again(se), or hold(h)?")
-    if press == "o":
-        off()
-    elif press == "sp":
-        spiral()
-    elif press == "w":
-        weave()
-    elif press == "c":
-        circle()
-    elif press == "se":
-        sence()
-    elif press == "h":
-        on()
-    else:
-        again()
+    on()
     
 def again():
     press = input("we ask again, would you like to: turn it off(o), spiral(sp), weave(w), circle(c), sence(se), or hold(h)?")
@@ -156,7 +134,7 @@ def again():
     elif press == "sp":
         spiral()
     elif press == "w":
-        weave()
+        inter_weave()
     elif press == "c":
         circle()
     elif press == "se":
